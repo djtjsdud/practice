dd3d3b2d6b96d889b91912ff47b23124f1602695
djtjsdud <djwnsdn@gmail.com>
2024-08-03 22:38:20 +0900

    docs:테이블,패밀리,디엘  연습

--- js/dl.js
+++ js/dl.js
@@ -1,17 +1,8 @@
-// $(document).ready(function(){
-//         $('dl dt').click(function(){
-//             $('+dd', this).slideDown(500).siblings('dd').slideUp(500);
-//         });
-//         $('dl dd').click(function(){
-//             $(this).slideUp(500);
-//         });
-//     });
-
-$(function(){
-    $('dl dt').on('click', function(){
-        $('+dd', this).slideDown(500).siblings('dd').slideUp(500);
+$(document).ready(function(){
+    $('dl dt').on('click',function(){
+        $('+dd',this).slideDown(500).siblings('dd').slideUp(500);
     });
-    $('dl dd').on('click', function(){
+    $('dd').on('click',function(){
         $(this).slideUp(500);
     });
-});   
+});
