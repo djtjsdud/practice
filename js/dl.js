e4a7cc62b3723b0335ba37152d287d59d56ba70b
djtjsdud <djwnsdn@gmail.com>
2024-08-05 17:43:54 +0900

    feat:학원에서 연습

--- js/dl.js
+++ js/dl.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){
-    $('dl dt').on('click',function(){
-        $('+dd',this).slideDown(500).siblings('dd').slideUp(500);
-    });
-    $('dd').on('click',function(){
-        $(this).slideUp(500);
-    });
-});
+$(document).
