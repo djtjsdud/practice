a91e9c46ba491804e57065fa86b5b8c84b791c07
djtjsdud <djwnsdn@gmail.com>
2024-08-06 00:16:49 +0900

    feat:연습 실패

--- js/dl.js
+++ js/dl.js
@@ -1,1 +1,16 @@
-$(document).
+// $(function(){
+//     $('dl dt'). on('click',function(){
+//         $('+dd',this).slidedown(500).siblings('dd').slideup(500);
+//     });
+// });
+
+
+
+$(document).ready(function(){
+    $('dl dt').click(function(){
+        $('+dd', this).slideDown(500).siblings('dd').slideUp(500);
+    });
+    $('dl dd').click(function(){
+        $(this).slideUp(500);
+    });
+});  
