294b6be31432c9d06542b5784756a15a1b0009a1
djtjsdud <djwnsdn@gmail.com>
2024-08-06 17:39:21 +0900

    feat:이미지 코드 연습

--- js/dl.js
+++ js/dl.js
@@ -1,16 +1,17 @@
-// $(function(){
-//     $('dl dt'). on('click',function(){
-//         $('+dd',this).slidedown(500).siblings('dd').slideup(500);
+// $(document).ready(function(){
+//     $('dl dt').click(function(){
+//         $('+dd', this).slideDown(500).siblings('dd').slideUp(500);
 //     });
-// });
+//     $('dl dd').click(function(){
+//         $(this).slideUp(500);
+//     });
+// });  
 
-
-
-$(document).ready(function(){
-    $('dl dt').click(function(){
+$(function(){
+    $('dl dt').on('click', function(){
         $('+dd', this).slideDown(500).siblings('dd').slideUp(500);
-    });
-    $('dl dd').click(function(){
+    })
+    $('dd').on('click', function(){
         $(this).slideUp(500);
-    });
-});  
+    })
+});
