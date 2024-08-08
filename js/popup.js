ffa3fb2841bb357e4617df10a8c6d7b459224a08
djtjsdud <djwnsdn@gmail.com>
2024-08-08 15:40:24 +0900

    feat:스티키, 팝업 코드 수업

---
+++ js/popup.js
@@ -0,0 +1,36 @@
+// $(function(){
+//     $('#open-btn').on('click', function(){
+//         $('#popup-box').show();
+//     });
+//     $('#popup-box > span').on('click', function(){
+//         $('#popup-box').hide();
+//     });
+// });
+
+
+// $(function(){
+//     $('#open-btn').on('click', function(){
+//         $('#popup-box').fadeIn(1000);
+//     });
+//     $('#popup-box > span').on('click', function(){
+//         $('#popup-box').fadeOut(1000);
+//     });
+// });
+
+// $(function(){
+//     $('#open-btn').on('click', function(){
+//         $('#popup-box').slideDown(1000);
+//     });
+//     $('#popup-box > span').on('click', function(){
+//         $('#popup-box').slideUp(1000);
+//     });
+// });
+
+$(function(){
+    $('#open-btn').on('click', function(){
+        $('#popup-box').addClass('slide')
+    });
+    $('#popup-box > span').on('click', function(){
+        $('#popup-box').removeClass('slide')
+    });
+});
