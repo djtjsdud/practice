8806ad3f745e4241c6eb25b31765be633c74d316
djtjsdud <djwnsdn@gmail.com>
2024-08-21 16:44:46 +0900

    feat:8/21 수업 내용 추가

--- js/key.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.stop-btn').on('click', function(){
-        $('#ball').css({'animationPlayState' : 'paused'});
-    });
-    $('.start-btn').on('click', function(){
-        $('#ball').css({'animationPlayState' : 'running', 'backgroundColor' : '#333'});
-    });
-});
